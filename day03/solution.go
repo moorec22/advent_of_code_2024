131f79b277c5206285550d55391378620f089591
Connor <moorec104@gmail.com>
2024-12-09 20:53:44 -0800

    [Day03] Add comments for day 3

--- day03/solution.go
+++ day03/solution.go
@@ -1,3 +1,14 @@
+// Advent of Code, 2024, Day 3
+//
+// https://adventofcode.com/2024/day/3
+//
+// For this problem I chose to implement a simple interpreter that can handle
+// the instructions given in the input file. The interpreter will scan the
+// input for the following instructions (dismissing any junk):
+//
+// - Multiply: multiplies the current value by the given number
+// - Do: sets the interpreter to execute Multiply instructions
+// - Don't: sets the interpreter to ignore Multiply instructions
 package day03
 
 import (
@@ -22,6 +33,8 @@ func PartTwoAnswer(filepath string) (int, error) {
 	return runProgramWithMatchers(filepath, matchers)
 }
 
+// runProgramWithMatchers runs the interpreter with the given matchers and
+// returns the final value of the program.
 func runProgramWithMatchers(filepath string, matchers []interpreter.Matcher) (int, error) {
 	answer := 0
 	err := util.ProcessFile(filepath, func(s *bufio.Scanner) error {
