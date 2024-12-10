0a86a2efe503267a7422066764ebc16c587aa35d
Connor <moorec104@gmail.com>
2024-12-09 23:40:20 -0800

    set up for day 5

---
+++ day05/solution.go
@@ -0,0 +1,12 @@
+// Advent of Code, 2024, Day 5
+//
+// https://adventofcode.com/2024/day/5
+package day05
+
+func PartOneAnswer(filepath string) (int, error) {
+	return 0, nil
+}
+
+func PartTwoAnswer(filepath string) (int, error) {
+	return 0, nil
+}
