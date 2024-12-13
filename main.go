80f0e89b499490fe6d08b8488931e3e93f797982
Connor <moorec104@gmail.com>
2024-12-12 22:19:31 -0800

    wrote dynamic implementation of choosing which day to run

--- main.go
+++ main.go
@@ -1,23 +1,30 @@
 package main
 
 import (
+	"advent/day01"
+	"advent/day02"
+	"advent/day03"
+	"advent/day04"
+	"advent/day05"
 	"advent/day06"
 	"advent/util"
 	"flag"
 	"fmt"
 )
 
-const FilePrefix = "day06/files/"
-const TestFile = "test.txt"
-const InputFile = "input.txt"
+const FilePrefix = "day%s/files/%s.txt"
+const TestFileName = "test"
+const InputFileName = "input"
 
 func main() {
-	testFlag := setUpTestFlag()
-	filepath := getFilepath(*testFlag)
+	testFlag, dayFlag := setUpFlags()
+	if *dayFlag <= 0 {
+		fmt.Println("Day number must be greater than 0")
+		return
+	}
+	filepath := getFilepath(*dayFlag, *testFlag)
 
-	var solution util.Solution
-
-	solution, err := day06.NewDay06Solution(filepath)
+	solution, err := SolutionFactories[*dayFlag](filepath)
 
 	if err != nil {
 		fmt.Printf("Error creating solution: %s\n", err)
@@ -39,15 +46,60 @@ func main() {
 	fmt.Printf("Part 2 answer: %d\n", answer)
 }
 
-func setUpTestFlag() *bool {
+// setUpTestFlag sets up the test flag and the day number, and returns them.
+func setUpFlags() (*bool, *int) {
 	testFlag := flag.Bool("t", false, "run with test.txt")
+	dayFlag := flag.Int("d", -1, "day number")
 	flag.Parse()
-	return testFlag
+	return testFlag, dayFlag
 }
 
-func getFilepath(testFlag bool) string {
+func getFilepath(day int, testFlag bool) string {
+	filename := "intput"
 	if testFlag {
-		return FilePrefix + TestFile
+		filename = "test"
 	}
-	return FilePrefix + InputFile
+	return fmt.Sprintf(FilePrefix, getTwoDigitNumber(day), filename)
+}
+
+func getTwoDigitNumber(n int) string {
+	if n < 10 {
+		return fmt.Sprintf("0%d", n)
+	}
+	return fmt.Sprintf("%d", n)
+}
+
+type SolutionFactory func(string) (util.Solution, error)
+
+var SolutionFactories = map[int]SolutionFactory{
+	1: Day01SolutionFactory,
+	2: Day02SolutionFactory,
+	3: Day03SolutionFactory,
+	4: Day04SolutionFactory,
+	5: Day05SolutionFactory,
+	6: Day06SolutionFactory,
+}
+
+func Day01SolutionFactory(filepath string) (util.Solution, error) {
+	return day01.NewDay01Solution(filepath)
+}
+
+func Day02SolutionFactory(filepath string) (util.Solution, error) {
+	return day02.NewDay02Solution(filepath)
+}
+
+func Day03SolutionFactory(filepath string) (util.Solution, error) {
+	return day03.NewDay03Solution(filepath)
+}
+
+func Day04SolutionFactory(filepath string) (util.Solution, error) {
+	return day04.NewDay04Solution(filepath)
+}
+
+func Day05SolutionFactory(filepath string) (util.Solution, error) {
+	return day05.NewDay05Solution(filepath)
+}
+
+func Day06SolutionFactory(filepath string) (util.Solution, error) {
+	return day06.NewDay06Solution(filepath)
 }
