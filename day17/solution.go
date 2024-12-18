5478c1b0a5cf1ddb4faf463bb3f80a9b8e5b160a
Connor <moorec104@gmail.com>
2024-12-18 11:17:59 -0800

    set up scaffolding for day 17 solution

---
+++ day17/solution.go
@@ -0,0 +1,18 @@
+// Advent of Code, 2024, Day 17
+//
+// https://adventofcode.com/2024/day/17
+package day17
+
+type Day17Solution struct{}
+
+func NewDay17Solution(filename string) (*Day17Solution, error) {
+	return &Day17Solution{}, nil
+}
+
+func (s *Day17Solution) PartOneAnswer() (int, error) {
+	return 0, nil
+}
+
+func (s *Day17Solution) PartTwoAnswer() (int, error) {
+	return 0, nil
+}
