5f4fbe6dd3232d026110dbe2b88ae1e8dfcdf60c
Connor <moorec104@gmail.com>
2024-12-20 14:49:56 -0800

    wrote scaffolding for day 19

---
+++ day19/solution.go
@@ -0,0 +1,23 @@
+// Advent of Code, 2024, Day 19
+//
+// https://adventofcode.com/2024/day/19
+//
+// Part 1: Deciding if a combination of towel stripe colors is possible or not
+// can be accomplished with recursion. This gives a runtime of O(k*2^n), where
+// k is the number of designs and n is the length of each design.
+// We could also use dynamic programming,
+package day19
+
+type Day19Solution struct{}
+
+func NewDay19Solution(filename string) (*Day19Solution, error) {
+	return &Day19Solution{}, nil
+}
+
+func (s *Day19Solution) PartOneAnswer() (int, error) {
+	return 0, nil
+}
+
+func (s *Day19Solution) PartTwoAnswer() (int, error) {
+	return 0, nil
+}
