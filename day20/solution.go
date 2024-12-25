f80039bd999479473e1baddc9dccf05da3c4a8ee
Connor <moorec104@gmail.com>
2024-12-24 17:08:08 -0800

    wrote scaffolding for day 20

---
+++ day20/solution.go
@@ -0,0 +1,18 @@
+// Advent of Code, 2024, Day 20
+//
+// https://adventofcode.com/2024/day/20
+package day20
+
+type Day20Solution struct{}
+
+func NewDay20Solution(filename string) (*Day20Solution, error) {
+	return &Day20Solution{}, nil
+}
+
+func (s *Day20Solution) PartOneAnswer() (int, error) {
+	return 0, nil
+}
+
+func (s *Day20Solution) PartTwoAnswer() (int, error) {
+	return 0, nil
+}
