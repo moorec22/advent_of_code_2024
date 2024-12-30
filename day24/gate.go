f08de34359b42c803775bf78a21f0108dd7a7bf5
Connor <moorec104@gmail.com>
2024-12-29 18:20:38 -0800

    have structure for circuit

---
+++ day24/gate.go
@@ -0,0 +1,35 @@
+package day24
+
+type Gate struct {
+	// variables that are input into a gate
+	Left, Right string
+	Compute     GateFunc
+}
+
+func NewGate(left, right string, compute GateFunc) *Gate {
+	return &Gate{left, right, compute}
+}
+
+// a simple, 2 input gate interface
+type GateFunc func(x, y bool) bool
+
+// AndGate returns true iff x && y
+func AndGate(x, y bool) bool {
+	return x && y
+}
+
+// OrGate returns true iff x || y
+func OrGate(x, y bool) bool {
+	return x || y
+}
+
+// XorGate returns true iff x != y
+func XorGate(x, y bool) bool {
+	return x != y
+}
+
+var GateFunctions = map[string]GateFunc{
+	"AND": AndGate,
+	"OR":  OrGate,
+	"XOR": XorGate,
+}
--- day24/solution.go
+++ day24/solution.go
@@ -1,12 +1,64 @@
 // Advent of Code, 2024, Day 24
 //
 // https://adventofcode.com/2024/day/24
+//
+// Part 1: The biggest challenge initially I think is how to store these gates.
+// My idea is to have a gate interface, for all types of gates. These gates do
+// not store state. Then, we'll have a circuit class, that stores all gates and
+// caches state for each gate. The circuit can compute the final output of all
+// z gates.
 package day24
 
-type Day24Solution struct{}
+import (
+	"advent/util"
+	"bufio"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type Day24Solution struct {
+	circuit   *Circuit
+	variables map[string]bool
+}
 
 func NewDay24Solution(filename string) (*Day24Solution, error) {
-	return &Day24Solution{}, nil
+	initialStates := make(map[string]bool)
+	gates := make(map[string]*Gate)
+	variables := make(map[string]bool)
+	err := util.ProcessFile(filename, func(scanner *bufio.Scanner) error {
+		// starting with inputs
+		for scanner.Scan() {
+			if scanner.Text() == "" {
+				break
+			}
+			parts := strings.Split(scanner.Text(), ": ")
+			state, err := strconv.Atoi(parts[1])
+			if err != nil || (state != 0 && state != 1) {
+				return fmt.Errorf("can't parse into boolean: '%s'", parts[1])
+			}
+			initialStates[parts[0]] = state == 1
+			variables[parts[0]] = true
+		}
+		// and get gates
+		for scanner.Scan() {
+			parts := strings.Split(scanner.Text(), "->")
+			gateParts := strings.Split(strings.TrimSpace(parts[0]), " ")
+			gateFunc, ok := GateFunctions[gateParts[1]]
+			if !ok {
+				return fmt.Errorf("could not find gate function for %s", gateParts[1])
+			}
+			outputVar := strings.TrimSpace(parts[1])
+			gates[outputVar] = NewGate(gateParts[0], gateParts[2], gateFunc)
+			variables[outputVar] = true
+		}
+		return scanner.Err()
+	})
+	if err != nil {
+		return nil, err
+	}
+	circuit := NewCircuit(initialStates, gates)
+	return &Day24Solution{circuit, variables}, nil
 }
 
 func (s *Day24Solution) PartOneAnswer() (int, error) {
